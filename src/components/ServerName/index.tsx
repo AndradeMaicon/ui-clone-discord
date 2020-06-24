cefb9e1c196111015fed968844fd71b35b1e399f
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-06-23 22:03:07 -0300

    Server name component

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import { Container, Title, ExplandIcon } from './styles';
+
+const ServerName: React.FC = () =>  {
+  return (
+    <Container>
+      <Title>Forte da Galera</Title>
+      <ExplandIcon />
+    </Container>
+  )
+};
+
+export default ServerName
