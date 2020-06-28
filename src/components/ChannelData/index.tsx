230a9f0e120012beac421cf25e2dbd217c43e0d8
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-06-28 14:20:56 -0300

    Recent message

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { Container, Message, InputWrapper, Input, InputIcon } from './styles';
 
 import ChannelMessage, { Mention } from '../ChannelMessage';
 
 const ChannelData: React.FC = () =>  {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef])
+
   return (
     <Container>
-      <Message>
-        <ChannelMessage 
-          author='Maicon'
-          date='28/06/2020'
-          content='Fala comigo!'
-        />
-
+      <Message ref={messagesRef} >
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage 
+            key={n}
+            author='Maicon'
+            date='28/06/2020'
+            content='Fala comigo!'
+          />
+        ))}
+        
         <ChannelMessage 
           author='Jimmy'
           date='28/06/2020'
