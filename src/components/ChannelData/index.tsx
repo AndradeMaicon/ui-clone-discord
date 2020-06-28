4ceb69170c624cfe2c9c27b0961bade7f84851bf
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-06-28 14:07:53 -0300

    Channal message

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 
 import { Container, Message, InputWrapper, Input, InputIcon } from './styles';
 
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
 const ChannelData: React.FC = () =>  {
   return (
     <Container>
-      <Message />
+      <Message>
+        <ChannelMessage 
+          author='Maicon'
+          date='28/06/2020'
+          content='Fala comigo!'
+        />
+
+        <ChannelMessage 
+          author='Jimmy'
+          date='28/06/2020'
+          content={
+            <>
+              <Mention>@kenob</Mention> vai jogar jovem?
+            </>
+          }
+          hasMention
+          isBot
+        /> 
+      </Message>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #geral" />
